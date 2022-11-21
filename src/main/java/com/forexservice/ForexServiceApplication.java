4b9710604142c911101d2387b4573a177684232e
Shivamshukla1234 <54464551+Shivamshukla1234@users.noreply.github.com>
2022-11-21 13:08:37 +0530

    Code-update

--- src/main/java/com/forexservice/ForexServiceApplication.java
+++ src/main/java/com/forexservice/ForexServiceApplication.java
@@ -10,12 +10,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
+
 public class ForexServiceApplication {
-     @GetMapping("/message")
-	public String getMessage(){
-		return "Welcome to Forex-Service..";
-	}
+
 
  public static  Logger logger =LoggerFactory.getLogger(ForexServiceApplication.class);
  @PostConstruct
